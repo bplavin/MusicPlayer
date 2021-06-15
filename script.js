ddb13e6645352e24255d7bdd7eb15adadc098044
Bogdan Plavin <bohdan.plavin@gmail.com>
2021-06-15 15:26:10 -0500

    Progress setup, song ending

--- script.js
+++ script.js
@@ -69,6 +69,24 @@ function pauseSong() {
     audio.pause();
 }
 
+// Update progress bar
+function updateProgress(e) {
+    const {duration, currentTime} = e.srcElement;
+    const progressPercent = (currentTime / duration) * 100;
+    progress.style.width = `${progressPercent}%`;
+
+    console.log(e.srcElement);
+}
+
+// Set progress bar
+function setProgress(e) {
+    const width = this.clientWidth; 
+    const clickX = e.offsetX;
+    const duration = audio.duration;
+
+    audio.currentTime = (clickX / width) * duration;
+}
+
 // Event listeners 
 playBtn.addEventListener('click', () => {
     const isPlaying = musicContainer.classList.contains('play');
@@ -86,3 +104,9 @@ nextBtn.addEventListener('click', nextSong);
 
 // Time/song update
 audio.addEventListener('timeupdate', updateProgress);
+
+// Click on the progress bar 
+progressContainer.addEventListener('click', setProgress);
+
+// Song ends 
+audio.addEventListener('ended', nextSong);
